59fde896cf758d79c2df0573d48c3f44ce8dde85
insertSoul <j.clarke213@gmail.com>
2023-11-11 16:07:30 +0000

    Test: Valid CocktailData.fromJson() object

--- random_cocktail_generator/test/widget_test.dart
+++ random_cocktail_generator/test/widget_test.dart
@@ -1,30 +1,29 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:random_cocktail_generator/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  group('CocktailData Tests', () {
+    test('CocktailData.fromJson() creates a valid object', () {
+      final json = {
+        'strDrink': 'Tequila Shot',
+        'strGlass': 'Shot glass',
+        'strDrinkThumb':
+            'https:\/\/www.thecocktaildb.com\/images\/media\/drink\/ywtrvt1441246783.jpg',
+        'strInstructions': 'Pour and enjoy!',
+        'strIngredient1': 'Tequila',
+        'strMeasure1': '25 ml',
+      };
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      final cocktailData = CocktailData.fromJson(json);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(cocktailData.strDrink, equals('Tequila Shot'));
+      expect(cocktailData.strGlass, equals('Shot glass'));
+      expect(cocktailData.strDrinkThumb,equals('https:\/\/www.thecocktaildb.com\/images\/media\/drink\/ywtrvt1441246783.jpg'));
+      expect(cocktailData.strInstructions, equals('Pour and enjoy!'));
+      expect(cocktailData.ingredients, equals(['Tequila']));
+      expect(cocktailData.measures, equals(['25 ml']));
+    });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    ;
   });
 }
