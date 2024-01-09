04de8dd36e3f7d7103e6e7adac57256e49720301
Jack-Clarke21 <Jack@shape.tech>
2024-01-09 16:24:30 +0000

    Style: Center and color loading wheel

--- random_cocktail_generator/lib/Pages/MainPage.dart
+++ random_cocktail_generator/lib/Pages/MainPage.dart
@@ -5,9 +5,7 @@ import 'package:random_cocktail_generator/Widgets/AppColors.dart';
 import 'package:random_cocktail_generator/Widgets/Buttons.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage ({super.key})  {
-
-  }
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -15,94 +13,103 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   CocktailData? data;
+
   @override
   void initState() {
     super.initState();
 
- fetchCocktailData().then((response){
-    setState((){data=response;});
+    fetchCocktailData().then((response) {
+      setState(() {
+        data = response;
+      });
     });
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.getColor(AppColor.backgroundColor),
       appBar: AppBar(
         title: const Text('Cocktail Details'),
       ),
-      body: data == null ? CircularProgressIndicator() : Center(
-        child: Column(
-          children: [
-            Text(
-              data!.strDrink,
-              style: TextStyle(
-                color: AppColors.getColor(AppColor.textColor),
-                fontSize: 50
-                ),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Made in a ${data!.strGlass}',
-              style: TextStyle(
-                color: AppColors.getColor(AppColor.textColor),
-                 fontSize: 20,
-                 ),
-            ),
-            SlideTransition(
-              position: Tween<Offset>(
-                begin: const Offset(-3.0, 0.0), // Slide from the left
-                end: Offset.zero,
-              ).animate(CurvedAnimation(
-                curve: Curves.easeOut,
-                parent: ModalRoute.of(context)!.animation!,
-              )),
-              child: Hero(
-                tag: 'cocktailImage_${data!.strDrink}',
-                child: Container(
-                  width: 340,
-                  height: 340,
-                  margin: const EdgeInsets.only(top: 5.0),
-                  decoration: const BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromARGB(108, 156, 113, 217),
-                        spreadRadius: 10,
-                        blurRadius: 80,
-                        offset: Offset(0, 15)
-                      ),
-                    ],
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(data!.strDrinkThumb),
-                  ) 
-                ),
+      body: data == null
+          ?  Center(
+              child: CircularProgressIndicator(
+                color: AppColors.getColor(AppColor.secondaryColor),
               ),
-            ),
-            Center(
+            )
+          : Center(
               child: Column(
                 children: [
-                  NavigateToCocktailInfoButton(data: data!),
+                  Text(
+                    data!.strDrink,
+                    style: TextStyle(
+                      color: AppColors.getColor(AppColor.textColor),
+                      fontSize: 50,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'Made in a ${data!.strGlass}',
+                    style: TextStyle(
+                      color: AppColors.getColor(AppColor.textColor),
+                      fontSize: 20,
+                    ),
+                  ),
                   SlideTransition(
                     position: Tween<Offset>(
-                      begin: const Offset(-0.8, 0), // Slide from the left
+                      begin: const Offset(-3.0, 0.0), // Slide from the left
                       end: Offset.zero,
                     ).animate(CurvedAnimation(
                       curve: Curves.easeOut,
                       parent: ModalRoute.of(context)!.animation!,
                     )),
-                    child: const Hero(
-                      tag:"NavigateToMainPageButton", 
-                      child: NavigateToMainPageButton()
+                    child: Hero(
+                      tag: 'cocktailImage_${data!.strDrink}',
+                      child: Container(
+                        width: 340,
+                        height: 340,
+                        margin: const EdgeInsets.only(top: 5.0),
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(108, 156, 113, 217),
+                              spreadRadius: 10,
+                              blurRadius: 80,
+                              offset: Offset(0, 15),
+                            ),
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(data!.strDrinkThumb),
+                        ),
                       ),
+                    ),
                   ),
+                  Center(
+                    child: Column(
+                      children: [
+                        NavigateToCocktailInfoButton(data: data!),
+                        SlideTransition(
+                          position: Tween<Offset>(
+                            begin: const Offset(-0.8, 0), // Slide from the left
+                            end: Offset.zero,
+                          ).animate(CurvedAnimation(
+                            curve: Curves.easeOut,
+                            parent: ModalRoute.of(context)!.animation!,
+                          )),
+                          child: const Hero(
+                            tag: "NavigateToMainPageButton",
+                            child: NavigateToMainPageButton(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
     );
   }
 }
-
